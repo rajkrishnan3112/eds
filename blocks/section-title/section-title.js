6d1bdef59625fbcb87b96008bb3ce9f1e3925693
rajkrishnanr3112 <rajkrishnanwebglo@gmail.com>
2026-09-23 17:24:35 +0530

    section title error fixed

--- blocks/section-title/section-title.js
+++ blocks/section-title/section-title.js
@@ -1,4 +1,4 @@
-export default function decorate(block) {
-  const items = [...block.children];
-  block.classList.add('section-title');
-}
+// export default function decorate(block) {
+//   const items = [...block.children];
+//   block.classList.add('section-title');
+// }
